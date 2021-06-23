47e104cbbae42e2dca98d384612cf7b3f9ad46cd
YuriyZ <yzabrovarniy@gmail.com>
2021-06-23 17:26:15 +0300

    fix(4.3.0) : added missed parethesis
    https://github.com/JanssenProject/jans-orm/issues/25

--- couchbase/src/main/java/org/gluu/orm/couchbase/impl/CouchbaseEntryManager.java
+++ couchbase/src/main/java/org/gluu/orm/couchbase/impl/CouchbaseEntryManager.java
@@ -95,7 +95,7 @@ public class CouchbaseEntryManager extends BaseEntryManager implements Serializa
 
         // if expiration is more then 30 days we must convert it to absolute Unit time stamp to avoid immediate expiration https://docs.couchbase.com/java-sdk/current/concept-docs/documents.html#setting-document-expiration
         if (value != null && value >= EXPIRATION_30_DAYS) {
-            final int now = (int) System.currentTimeMillis() / 1000;
+            final int now = (int) (System.currentTimeMillis() / 1000);
             value = now + value;
         }
 
