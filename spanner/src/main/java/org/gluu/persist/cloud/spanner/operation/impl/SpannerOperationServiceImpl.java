490d34d5d6b0b68d75a317e747e662f4c999fd0c
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2022-08-18 21:43:38 +0300

    fix: don't execute next paged search if current result count less than (Jans #2171)

--- spanner/src/main/java/org/gluu/persist/cloud/spanner/operation/impl/SpannerOperationServiceImpl.java
+++ spanner/src/main/java/org/gluu/persist/cloud/spanner/operation/impl/SpannerOperationServiceImpl.java
@@ -651,7 +651,7 @@ public class SpannerOperationServiceImpl implements SpannerOperationService {
 	
 	                    resultCount += lastCountRows;
 	
-	                    if ((count > 0) && (resultCount >= count)) {
+	                    if ((count > 0) && (resultCount >= count) || (lastCountRows < currentLimit)) {
 	                        break;
 	                    }
 	                } while (lastCountRows > 0);
